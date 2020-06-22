7a705fc6756e9fd743a06bdd5edb3c1c83ebd27c
Maicol Felipe Duque Urrea <maicolduque01@gmail.com>
2020-06-22 14:20:28 -0500

    Config initial

---
+++ src/components/Header/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './estilos.css'
+
+export default () => (
+  <header className="header">
+    HEADER
+  </header>
+)
---
+++ src/components/Menu/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './estilos.css'
+
+export default () => (
+  <div className="menu">
+    <div className="content-menu">
+      MENU
+    </div>
+  </div>
+)
